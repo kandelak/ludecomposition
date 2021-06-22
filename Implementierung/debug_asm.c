8e5be7e92fe5ea85cb4b4e4020800e1859892cdf
Benjamin Liertz <ge78zig@mytum.de>
2021-06-22 19:56:08 +0200

    erste ASM-Version

---
+++ Implementierung/debug_asm.c
@@ -0,0 +1,22 @@
+#include "test.h"
+//#include "ludecomp_asm.h"
+
+void ludecomp_asm(size_t n, const float* A, float* L, float* U, float *P);
+
+
+int main(int argc, char** argv) {
+    size_t n = 3;
+    float L[n * n];
+    float U[n * n];
+    float P[n * n];
+    float A[] = {1,2,3,4,5,6,7,8,9};
+    ludecomp_asm(n,A,L,U,P);
+    printf("Matrix A\n");
+    printMatrix(n,A);
+    printf("Matrix L\n");
+    printMatrix(n,L);
+    printf("Matrix U\n");
+    printMatrix(n,U);
+    printf("Matrix P\n");
+    printMatrix(n,P);
+}
