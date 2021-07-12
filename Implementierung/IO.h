230ba57ebd18b5170bf2ddeb5da05033346ab210
Aleksandre Kandelaki <ge54not@mytum.de>
2021-07-12 12:20:29 +0200

    headers added

---
+++ Implementierung/IO.h
@@ -0,0 +1,237 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define LINE_SEPARATOR " \n############################################### \n\n"
+
+void printHelp()
+{
+    printf("\n--help/-h : Display brief summaries of command line Options\n\n");
+    printf("--bench-all : Generates and runs benchmark on the pre-defined input configuration (in bench.txt)\n\n"
+           "       Example: ./main --bench-all\n\n"
+           "      Note: After this flag used you can then use the generated file for benchmarkin other implementations\n\n"
+           "       Example: ./main --input=bench.txt -v 2 --no-print -b\n\n");
+    printf("--input/-f: Specify input stream\n\n"
+           "       Example: ./main --input=input_file.txt\n"
+           "       Example: ./main --input input_file.txt\n"
+           "       Example: ./main -f=input_file.txt\n\n"
+           "       Note: Standart input stream will not be used as default or at all. Always specify input file"
+           "       How input works?\n\n"
+           "       Input for our program is just enter (new line) seperated numbers in the file\n\n"
+           "       First number is always integer and specifies the number of the matrices to be decomposed\n"
+           "       After that each and every time first comes the row/column size (Also integer) of the Matrix and then the matrix entries\n"
+           "       Note: Input validity check fails if these constraints are not kept. Extra input ,after all input are checked, will be ignored.");
+    printf("--no-print: Deactivates user-friendly printing. For instance for testing/benchmarking purposes or during computations on larger matrices.\n\n"
+           "       Example: ./main --input=input_file.txt --no-print\n\n"
+           "       Note: Always use this if the matrices are too large\n\n");
+    printf("-b: Activates Benchmarking\n\n"
+           "       Example: ./main --input=input_file.txt -b\n"
+           "       Example: ./main --input=input_file.txt -b --no-print\n\n");
+    printf("--output/-o: Chooses output stream for storing the results\n\n"
+           "       Note: Standart output stream is the default output stream\n\n"
+           "       Example: ./main --input=input_file.txt -b --output=results.txt\n\n");
+    printf("-v: Chooses the implementation for the decomposition\n\n"
+           "   Implementations:\n\n"
+           "       0 : C\n"
+           "       1 : C with intrinsics\n"
+           "       2 : Assembler with SIMD\n"
+           "       3 : Assembler\n"
+           "       4 : C without Pivoting\n\n"
+           "       Note: C is the default implementation\n\n"
+           "       Note: C without Pivoting is not working properly most of the time. Nevertheless you can still try to decompose with it\n\n"
+           "       Example: ./main --input=input_file.txt --output=results.txt -v 2\n"
+           "       Example: ./main -f input_file.txt -v 1 -b");
+    printf("-i : Choose number of iterations\n\n"
+           "       Note: If benchmarking the average time is computed automatically\n\n"
+           "       Example: ./main --input input_file.txt -o result.txt -b -v 3 -i 10\n\n");
+    printf("--test-all/-k : Tests chosen implementation with pre-defined input from predefined file (test.txt)\n\n");
+    printf("--random-test : Generates single random input with specified row/column size of the matrix (in file random_single.txt)\n\n"
+           "       Example: ./main --random-test 500\n"
+           "       This will generate 500x500 randomized matrix\n\n"
+           "       After that you can use this command to run the test:\n\n"
+           "       Example: ./main --input=random_single.txt --no-print -t\n"
+           "       Example: ./main --input=random_single.txt --no-print -t -v 2\n\n"
+           "       Or you can also run bench using this command and write results in output file:"
+           "       Example: ./main --input=random_single.txt --no-print -b -o result.txt -v 3\n\n");
+    printf("--random-tests : Generates multiple random inputs for testing purposes with specified(in file random_multiple.txt)\n\n"
+           "       Example: ./main --random-tests\n"
+           "       After that you can run tests again and write results in output file using this command:\n"
+           "       Example: ./main --input=random_multiple.txt --output=result.txt --no-print -t -v 2");
+    printf("--pivot/-p : Shows comparision example for decomposition with and without pivoting\n\n"
+           "       Example: ./main --pivot\n\n");
+}
+
+/**
+ * Checks validity of the input
+ */
+
+int check_validity(FILE *input)
+{
+    char buf[50];
+
+    size_t num_of_matr;
+
+    if (!fgets(buf, 50, input))
+    {
+        fprintf(stderr, "Reading Input Failed. (empty file)\n");
+        return 0;
+    }
+
+    errno = 0;
+
+    char *endptr;
+
+    num_of_matr = strtol(buf, &endptr, 10);
+
+    if (errno == ERANGE)
+    {
+        perror("Integer Number read is too small or too large.\n");
+        return 0;
+    }
+    else if (endptr == buf)
+    {
+        printf("Nothing Read (No input for the number of the matrices)\n");
+        return 0;
+    }
+    else if (*endptr && *endptr != '\n')
+    {
+        printf("Wrong Input Format (All numbers must be enter seperated). Please refer to --help/-h.\n");
+        return 0;
+    }
+    else if ((int)num_of_matr <= 0)
+    {
+        printf("Number of Matrices can not be less than or equal 0.\n");
+        return 0;
+    }
+
+    for (size_t k = 0; k < num_of_matr; k++)
+    {
+        if (!fgets(buf, 50, input))
+        {
+            fprintf(stderr, "Reading Input Failed. (less input than specified/empty line)\n");
+            return 0;
+        }
+
+        size_t size_of_matr_row = strtol(buf, &endptr, 10);
+
+        if (errno == ERANGE)
+        {
+            perror("Integer number read was too small or too large.\n");
+            return 0;
+        }
+        else if (endptr == buf)
+        {
+            printf("Nothing Read (No input for the Size of the Matrix)\n");
+            return 0;
+        }
+        else if (*endptr && *endptr != '\n')
+        {
+            printf("Wrong Input Format (All numbers must be enter seperated). Please refer to --help/-h.\n");
+            return 0;
+        }
+        else if ((int)size_of_matr_row <= 0)
+        {
+            printf("Row/Column size of the Matrix can not be less than or equal zero.\n");
+            return 0;
+        }
+
+        size_t size_of_matr = size_of_matr_row * size_of_matr_row;
+        for (size_t i = 0; i < size_of_matr; i++)
+        {
+            if (!fgets(buf, 50, input))
+            {
+                fprintf(stderr, "Reading Input Failed. (less input than specified/empty line)\n");
+                return 0;
+            }
+
+            float matr_entry = strtod(buf, &endptr);
+
+            if (errno == ERANGE)
+            {
+                perror("Floating-point Number read was too small or too large.\n");
+                return 0;
+            }
+            else if (endptr == buf)
+            {
+                printf("Nothing Read (Not enough entries for specified size of the Matrix)\n");
+                return 0;
+            }
+            else if (*endptr && *endptr != '\n')
+            {
+
+                printf("Wrong Input Format (Matrix Entry. All numbers must be enter seperated). Please refer to --help/-h.\n");
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+/**
+ * Writes Matrix M in the stream specified with out (Used for representing matrices to User)
+ */
+void write_pretty(FILE *out, size_t n, const float *M)
+{
+    for (size_t index = 0; index < n * n - 1; index++)
+    {
+        fprintf(out, "%f ", M[index]);
+        if ((index + 1) % n == 0)
+            fprintf(out, "\n");
+    }
+    fprintf(out, "%f\n\n", M[n * n - 1]);
+}
+
+#define STACK_LIMIT 700
+
+/**
+ * Reads matrix entries from the stream specified with fp and writes in matrix
+ */
+void read_matrix_from_stream(size_t n, FILE *fp, const float *matrix)
+{
+
+    size_t index = 0;
+
+    size_t mat_size = n * n;
+
+    while (mat_size > 0)
+    {
+        if (fscanf(fp, "%f", matrix + (index++)) == -1)
+        {
+            break;
+        }
+        mat_size--;
+    }
+}
+
+/**
+ * Writes Decomposition matrices in the stream specified with output
+ */
+void print_pretty(FILE *output, float *A, float *L, float *U, float *P, size_t size_of_matr_row, size_t i)
+{
+
+    fprintf(output, "\nOperation %ld: \n\n", i);
+    fprintf(output, " Matrix A: \n\n");
+    write_pretty(output, size_of_matr_row, A);
+    fprintf(output, " Matrix L: \n\n");
+    write_pretty(output, size_of_matr_row, L);
+
+    fprintf(output, " Matrix U: \n\n");
+    write_pretty(output, size_of_matr_row, U);
+
+    fprintf(output, " Matrix P: \n\n");
+    write_pretty(output, size_of_matr_row, P);
+    fprintf(output, LINE_SEPARATOR);
+}
+
+/**
+ * Writes Matrix M in the stream specified with out (Used for generating input file)
+ */
+void write_matrix_in_stream(FILE *out, size_t n, const float *M)
+{
+    for (size_t index = 0; index < n * n - 1; index++)
+    {
+        fprintf(out, "%f\n", M[index]);
+    }
+    fprintf(out, "%f\n", M[n * n - 1]);
+}
---
+++ Implementierung/generator.h
@@ -0,0 +1,268 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "IO.h"
+/**
+ * Worst Case input for Pivoting
+ */
+void must_pivotize(size_t n, float *A)
+{
+        size_t offset = n;
+        A[0] = 1;
+        for (size_t i = 1; i < n * n; i++)
+        {
+                if (i % offset == 0)
+                {
+                        A[i] = 5.0;
+                        offset += (n + 1);
+                }
+                else
+                {
+                        A[i] = 1.0;
+                }
+        }
+}
+
+/**
+ * Generates Matrix with entries from 0 to range
+ */
+void matrix_generator(size_t n, float *A, float range)
+{
+        srand((unsigned int)time(NULL));
+        for (size_t i = 0; i < n * n; i++)
+                A[i] = ((float)rand() / (RAND_MAX)) * range;
+}
+
+/**
+ * Generates Matrix with entries from 0 to range (negative numbers)
+ */
+void matrix_generator2(size_t n, float *A, float range)
+{
+        srand((unsigned int)time(NULL));
+        for (size_t i = 0; i < n * n; i++)
+        {
+                A[i] = ((float)rand() / (RAND_MAX)) * range;
+                int num = (int)A[i];
+                A[i] = (num % 7 > 4 ? 1 : -1) * A[i];
+        }
+}
+
+/**
+ * Generates Matrix with entries from 0 to range (no fraction part & negatives)
+ */
+void matrix_generator3(size_t n, float *A, float range)
+{
+        srand((unsigned int)time(NULL));
+        for (size_t i = 0; i < n * n; i++)
+        {
+                int num = (int)(((float)rand() / (RAND_MAX)) * range);
+                A[i] = (num % 7 > 4 ? 1 : -1) * num;
+        }
+}
+
+
+/**
+ * Generates non-decomposable Matrices
+ */
+
+void not_decomposable1(size_t n, float *A)
+{
+        for (size_t i = 0; i < n * n; i++)
+        {
+                A[i] = 0;
+        }
+}
+
+void not_decomposable2(size_t n, float *A)
+{
+        for (size_t i = 0; i < n * n; i++)
+        {
+                A[i] = 2.0;
+        }
+}
+
+void not_decomposable3(size_t n, float *A)
+{
+        for (size_t i = 0; i < (n * n) / 2; i++)
+        {
+                A[i] = 3.0;
+        }
+        for (size_t i = (n * n) / 2; i < (n * n); i++)
+        {
+                A[i] = 0;
+        }
+}
+
+void not_decomposable4(size_t n, float *A)
+{
+        for (size_t i = 0; i < (n * n) - 1; i++)
+        {
+                A[i] = i + 1;
+        }
+        for (size_t i = (n * n) - 1; i < (n * n); i++)
+        {
+                A[i] = i;
+        }
+}
+/**
+ * Generates multiple inputs for testing purposes
+ */
+
+#define TOLERATE_ERROR 2e-1
+#define MUST_ALIGN_HEAP 800
+
+void random_multiple_input(char *output)
+{
+
+        FILE *out;
+        {
+                out = fopen(output, "w");
+                if (!out)
+                {
+                        perror("Error occurred while opening file for generating the randomized inputs for Testing");
+                        exit(EXIT_FAILURE);
+                }
+        }
+
+        fprintf(out, "%ld\n", 65);
+
+        float range = 1000.0;
+
+        //Tests if the Heap Allocation happens accordingly
+        float *C;
+
+        C = malloc(sizeof(float) * MUST_ALIGN_HEAP * MUST_ALIGN_HEAP);
+
+        if (!C)
+        {
+                perror("Could not allocate Memory");
+                exit(EXIT_FAILURE);
+        }
+        fprintf(out, "%ld\n", MUST_ALIGN_HEAP);
+        matrix_generator(MUST_ALIGN_HEAP, C, range);
+        write_matrix_in_stream(out, MUST_ALIGN_HEAP, C);
+
+        free(C);
+
+        size_t next_size = 10;
+
+        fprintf(out, "%ld\n", next_size);
+        float B[next_size * next_size];
+        not_decomposable1(next_size, B);
+        write_matrix_in_stream(out, next_size, B);
+
+        fprintf(out, "%ld\n", next_size);
+        not_decomposable2(next_size, B);
+        write_matrix_in_stream(out, next_size, B);
+
+        fprintf(out, "%ld\n", next_size);
+        not_decomposable3(next_size, B);
+        write_matrix_in_stream(out, next_size, B);
+
+        fprintf(out, "%ld\n", next_size);
+        not_decomposable4(next_size, B);
+        write_matrix_in_stream(out, next_size, B);
+
+        range = 10000.0;
+        for (size_t k = 1; k <= 600; k += 20)
+        {
+                float *D = malloc(sizeof(float) * k * k);
+
+                if (!D)
+                {
+                        perror("Could not allocate Memory");
+                        exit(EXIT_FAILURE);
+                }
+                fprintf(out, "%ld\n", k);
+
+                matrix_generator2(k, D, range);
+
+                write_matrix_in_stream(out, k, D);
+
+                free(D);
+        }
+
+        for (size_t k = 1; k <= 600; k += 20)
+        {
+                float *D = malloc(sizeof(float) * k * k);
+
+                if (!D)
+                {
+                        perror("Could not allocate Memory");
+                        exit(EXIT_FAILURE);
+                }
+
+                fprintf(out, "%ld\n", k);
+                matrix_generator3(k, D, range);
+                write_matrix_in_stream(out, k, D);
+                free(D);
+        }
+
+        fclose(out);
+}
+
+/**
+ * Generates single random input
+ */
+void random_single_input(char *output, size_t n)
+{
+        FILE *out;
+        {
+                out = fopen(output, "w");
+                if (!out)
+                {
+                        perror("Error occurred while opening file for generating the randomized inputs for Testing");
+                        exit(EXIT_FAILURE);
+                }
+        }
+
+        float *A = malloc(sizeof(float) * n * n);
+
+        if (!A)
+        {
+                perror("Could not allocate Memory");
+                exit(EXIT_FAILURE);
+        }
+
+        fprintf(out, "%ld\n", 1);
+        matrix_generator(n, A, 1000.0);
+        fclose(out);
+}
+
+void generate_bench(char *bench_file)
+{
+        FILE *bench;
+        {
+                bench = fopen(bench_file, "w");
+                if (!bench)
+                {
+                        perror("Error occurred while opening file for generating the randomized inputs for Testing");
+                        exit(EXIT_FAILURE);
+                }
+        }
+        fprintf(bench, "%ld\n", 1000 / 25);
+        for (size_t i = 1; i < 1000; i += 50)
+        {
+                float *A;
+
+                A = malloc(sizeof(float) * i * i);
+
+                if (!A)
+                {
+                        perror("Could not allocate Memory");
+                        exit(EXIT_FAILURE);
+                }
+
+                matrix_generator(i, A, 1000.0);
+                fprintf(bench, "%ld\n", i);
+                write_matrix_in_stream(bench, i, A);
+
+                fprintf(bench, "%ld\n", i);
+                must_pivotize(i, A);
+                write_matrix_in_stream(bench, i, A);
+                free(A);
+        }
+
+        fclose(bench);
+}
